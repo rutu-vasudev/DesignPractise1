2a3be249358d147362c5d3545bf9d85986aadd83
rutu-vasudev <122589643+rutu-vasudev@users.noreply.github.com>
2023-02-24 11:53:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mindbodyclone/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mindbodyclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+
+        setContentView(R.layout.activity_main)
+    }
+}
